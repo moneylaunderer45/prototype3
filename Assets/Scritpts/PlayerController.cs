45bdaf74cd7a8465545cf039e009c45c6c4bbde3
moneylaunderer45 <146860802+moneylaunderer45@users.noreply.github.com>
2023-10-03 10:04:11 -0700

    3.1 step 8

---
+++ Assets/Scritpts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityModifier;
+    void Start() {
+            playerRb = GetComponent<Rigidbody>();
+            Physics.gravity *= gravityModifier; }
+
+    // Update is called once per frame
+    public bool isOnGround = true;
+    void Update(){
+    if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+      playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false; } }
+
+    private void OnCollisionEnter(Collision collision) {
+      isOnGround = true; }
+} 
